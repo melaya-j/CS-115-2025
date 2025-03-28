41dfe9d41b28a616d4895292be1c77e3fca36e10
melaya-j <melaya710@gmail.com>
2025-03-28 11:00:14 -0500

    SET 10 Q6

---
+++ numCalculate1.java
@@ -0,0 +1,14 @@
+// Question 6
+public class numCalculate1 {
+    public static void main(String[] args) {
+        Double calcRet = calculate;
+        calculate(8.0, 9.0);
+        System.out.println( Calculate results are: + calcRet);
+}
+
+
+public static void calculate(double numb1,double numb2) {
+    double result = numb1 * numb2; 
+    System.out.println("Outcome: "+ result);  
+}
+}
