bc2695a948c0ef479d7dd3a2b22043b572c42343
melaya-j <melaya710@gmail.com>
2025-03-28 10:50:21 -0500

    SET 10 Q 5

---
+++ numCalculate.java
@@ -0,0 +1,12 @@
+public class numCalculate {
+
+        public static void main(String[] args) 
+        {
+        calculate(3.0, 5.0);
+    }
+
+
+    public static void calculate(double numb1,double numb2) {
+        double result = numb1 * numb2; 
+        System.out.println("Outcome: "+ result);  
+}}
